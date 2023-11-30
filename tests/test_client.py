eb26567fd279a092370eede37c068ec9da9ae0b6
Felipe Sodré <felipe.b4rros@gmail.com>
2023-11-30 11:01:15 -0300

    conver patch e patch.object do test_client_load_states a context manager

--- tests/test_client.py
+++ tests/test_client.py
@@ -245,13 +245,11 @@ async def test_async_client_occurrences(occurrences_client_and_get_mock):
     )
 
 
-@patch("crossfire.clients.config")
-@patch.object(AsyncClient, "states")
-def test_client_load_states(async_states_mock, config_mock):
-    config_mock.side_effect = ("email", "password")
-    async_states_mock.return_value = ("forty-two", 42)
-    client = Client()
-    client.cached_token = Token("42", 3600)
-    client.URL = "http://127.0.0.1/api/v2"
-    client.states(format=None)
-    async_states_mock.assert_called_with(format=None)
+def test_client_load_states():
+    with patch("crossfire.clients.config") as config_mock:
+        with patch.object(AsyncClient, "states") as async_states_mock:
+            config_mock.side_effect = ("email", "password")
+            async_states_mock.return_value = ("forty-two", 42)
+            client = Client()
+            client.states(format=None)
+            async_states_mock.assert_called_with(format=None)

87175d89d5d281b4139ba6c860da9f40443cea16
Felipe Sodré <felipe.b4rros@gmail.com>
2024-02-22 18:03:29 -0300

    create test passing df to flatten :warning: not passing

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -1,4 +1,7 @@
+from geopandas import GeoDataFrame
+from pandas import DataFrame
 from pytest import raises
+from shapely import Point
 
 from crossfire import NestedColumnError, flatten
 
@@ -8,6 +11,9 @@ DICT_DATA = [
         "contextInfo": {"context1": "info1", "context2": "info2"},
     }
 ]
+PD_DATA = DataFrame(DICT_DATA)
+GEOMETRY = [Point(4, 2)]
+GEOPD_DATA = GeoDataFrame(DICT_DATA, crs="EPSG:4326", geometry=GEOMETRY)
 
 
 def teste_flatten_wrong_nested_columns_value_error():
@@ -31,3 +37,21 @@ def test_flatten_dict():
             "contextInfo_context2": "info2",
         }
     ]
+
+
+# Write a test that passes a pandas.DataFrame as data to the existing flatten function
+def test_flatten_gpd():
+    flattened_pd = flatten(
+        PD_DATA, nested_columns=["contextInfo", "neighborhood"]
+    )
+    assert flattened_pd.equal(
+        DataFrame(
+            [
+                {
+                    "answer": 42,
+                    "contextInfo_context1": "info1",
+                    "contextInfo_context2": "info2",
+                }
+            ]
+        )
+    )
